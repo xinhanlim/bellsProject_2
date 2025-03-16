e83d81919f769fd4d4da53a80d2595d0726221fb
xinhanlim <xhxhan00@gmail.com>
2025-03-16 21:04:28 +0800

    added introduction and give console log to both

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ window.addEventListener('load', function(){
     let createReceipeBtn = document.querySelector(".createReceipeBtn");
 
     createReceipeBtn.addEventListener('click', function(){
-        let receipeNameInput = document.querySelector("input[name='receipeName']")
+        let receipeNameInput = document.querySelector("input[name='receipeName']");
+        let receipeName = receipeNameInput.value;
+        console.log(receipeName);
+
+        let introductionInput = document.querySelector("input[name'introduction']");
+        let introduction = introductionInput.value;
+        console.log(introduction);
+
         
 
     })
