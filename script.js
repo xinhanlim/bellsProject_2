d09fc07cd839d41a91486b313782ce6c38f005e3
xinhanlim <xhxhan00@gmail.com>
2025-03-16 21:11:10 +0800

    fix spelling error for Recipe

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 window.addEventListener('load', function(){
-    let createReceipeBtn = document.querySelector(".createReceipeBtn");
+    let createReceipeBtn = document.querySelector(".createRecipeBtn");
 
-    createReceipeBtn.addEventListener('click', function(){
-        let receipeNameInput = document.querySelector("input[name='receipeName']");
-        let receipeName = receipeNameInput.value;
-        console.log(receipeName);
+    createReceipeBtn.addEventListener('click', function(){eceipe
+        let recipeNameInput = document.querySelector("input[name='rName']");
+        let recipeName = recipeNameInput.value;
+        console.log(recipeName);
 
-        let introductionInput = document.querySelector("input[name'introduction']");
+        let introductionInput = document.querySelector("input[name='introduction']");
         let introduction = introductionInput.value;
         console.log(introduction);
 
-        
+        let ingredientsInput = document.querySelector("input[name='ingredients']");
+        let ingredients = ingredientsInput.value;
+        console.log(ingredients);
+
+        let methodInput = document.querySelector("input[name='method']");
+        let method = methodInput.value;
+        console.log(method);
 
     })
     
