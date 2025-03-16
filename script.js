34aab69efa1d2aa6d080581cbcd2aab6fa461a9d
xinhanlim <xhxhan00@gmail.com>
2025-03-16 20:56:40 +0800

    added eventlistener to createreceipebtn

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.addEventListener('load', function(){
     let createReceipeBtn = document.querySelector(".createReceipeBtn");
 
     createReceipeBtn.addEventListener('click', function(){
+        let receipeNameInput = document.querySelector("input[name='receipeName']")
         
 
     })
