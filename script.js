eb111c846df777fd8883c8a1d9fcbfc34f524987
xinhanlim <xhxhan00@gmail.com>
2025-03-16 22:57:47 +0800

    added rules and criteria for the create file able to console.log all result

--- script.js
+++ script.js
@@ -3,32 +3,87 @@ window.addEventListener('load', function(){
 
     //Gather the input of user and console.log.
     submitBtn.addEventListener('click', function(){
-        let recipeImgInput = document.querySelector("#imgFormFile")
-        let recipeImg = recipeImgInput.files[0];
-        console.log(recipeImg);
-        let recipeImgURL = URL.createObjectURL(recipeImg)
-        console.log(recipeImgURL);
 
         // Rules / Criteria
-        
+        // recipeImg : need a photo to upload
+        // recipeName : (1) cannot be blank (2) must be more than 5 letters
+        let recipeNameIsBlank = false;
+        let recipeNameIsShort = false;
+        // introduction : (1) must be more than 10 words (2) cannot be blank 
+        let introductionIsShort = false;
+        let introductionIsBlank = false;
+        // ingredients: (1) must be more than 20 words (2) cannot be blank 
+        let ingredientsIsShort = false;
+        let ingredientsIsBlank = false;
+        // method : (1) must be more than 10 words (2) cannot be blank 
+        let methodIsShort = false;
+        let methodIsBlank = false;
 
+        let recipeImgInput = document.querySelector("#imgFormFile")
+        let recipeImg = recipeImgInput.files[0];
+        
+        let recipeImgURL = URL.createObjectURL(recipeImg)
+        console.log(recipeImgURL);
+        if (recipeImgIsBlank = recipeImg.length === 0) {
+            console.log("No image uploaded.");
+        } else {
+            let recipeImg = recipeImgInput.files[0]; // Get the uploaded file
+            console.log("Uploaded file:", recipeImg);
+        }
 
         let recipeNameInput = document.querySelector("input[name='recipeName']");
-        let recipeName = recipeNameInput.value;
-        console.log(recipeName);
+        let recipeNameText = recipeNameInput.value;
+        if(recipeNameText == ''){
+            recipeNameIsBlank = true;
+        }
+        if(recipeNameText <= 5){
+            recipeNameIsShort = true;
+        }
 
         let introductionInput = document.querySelector("textarea[name='introduction']");
-        let introduction = introductionInput.value;
-        console.log(introduction);
+        let introductionText = introductionInput.value;
+        if(introductionText == ''){
+            introductionIsBlank = true;
+        }
+        if(introductionText <= 10){
+            introductionIsShort = true;
+        }
 
         let ingredientsInput = document.querySelector("textarea[name='ingredients']");
-        let ingredients = ingredientsInput.value;
-        console.log(ingredients);
+        let ingredientsText = ingredientsInput.value;
+        if(ingredientsText == ''){
+            ingredientsIsBlank = true;
+        }
+        if(ingredientsText <= 20){
+            ingredientsIsShort = true;
+        }
 
         let methodInput = document.querySelector("textarea[name='method']");
-        let method = methodInput.value;
-        console.log(method);
+        let methodText = methodInput.value;
+        if(methodText == ''){
+            methodIsBlank = true;
+        }
+        if(methodText <= 10){
+            methodIsShort = true;
+        }
 
+
+        if(recipeImgIsBlank == false && 
+            recipeNameIsBlank == false &&
+            recipeNameIsShort == false &&
+            introductionIsBlank == false &&
+            introductionIsShort == false &&
+            ingredientsIsBlank == false &&
+            ingredientsIsShort == false &&
+            methodIsBlank == false &&
+            methodIsShort == false) {
+            //All information as uploaded based on requirements.
+            console.log(recipeImg);
+            console.log(recipeNameText);
+            console.log(introductionText);
+            console.log(ingredientsText);
+            console.log(methodText);
+            }
     })
     
 })
